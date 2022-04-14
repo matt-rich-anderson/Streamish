564517248446e9f2785f83a6f6516afeb360797b
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-04-14 13:07:23 -0500

    video with comments

--- Streamish/Controllers/VideoController.cs
+++ Streamish/Controllers/VideoController.cs
@@ -57,5 +57,11 @@ namespace Streamish.Controllers
             _videoRepository.Delete(id);
             return NoContent();
         }
+        [HttpGet("GetWithComments")]
+        public IActionResult GetWithComments()
+        {
+            var videos = _videoRepository.GetAllWithComments();
+            return Ok(videos);
+        }
     }
 }
